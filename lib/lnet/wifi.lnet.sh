2a3826e7a9e93f6f63c39558601e2b14c0519f7a
Dave Brown <dagbrown@lart.ca>
2024-02-23 13:00:14 +0900

    Add function to scan a wifi interface for APs

---
+++ lib/lnet/wifi.lnet.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Runs the "iwlist" utility on the device to scan for wireless APs
+#
+# Since iwlist has some wildly-inconsistent output, every single line of
+# its output has to be parsed separately.  This means that this is
+# probably REALLY fragile, because if the output was that fragile to
+# begine with, it's definitely subject to the whims of the developers of
+# iwlist.
+
+wifi_find_aps() {
+    local device=$1
+
+    iwlist $device scan | awk '
+        BEGIN {
+        }
+
+        # Cell 23 - Address: E2:B4:F7:99:56:C9
+        /Cell/ { 
+            if(cell_essid) {
+                print "WIFI_AP[" cell_num "]=" cell_essid
+                print "WIFI_QUALITY[" cell_num "]=" cell_quality
+                print "WIFI_ENCRYPTION[" cell_num "]=" cell_encryption
+            }
+            cell_num = $2
+        }
+
+        # Quality=25/70  Signal level=-85 dBm
+
+        /Quality/ {
+            OFS=FS
+            cell_quality=$1
+            FS="="
+            $0=cell_quality
+            cell_quality=$2
+            FS=OFS
+        }
+
+        # Encryption key:on
+
+        /Encryption/ {
+            if($2 == "key:on") {
+                cell_encryption="true"
+            } else {
+                cell_encryption="false"
+            }
+        }
+        
+        # ESSID:"xg100n-98598d-1"
+
+        /ESSID/ { 
+            OFS=FS
+            FS=":"
+            $0=$0 # force resplit of input
+            cell_essid=$2
+            FS=OFS
+        }
+        
+        END { # get the last one
+            if(cell_essid) {
+                print "WIFI_APS[" cell_num "]=" cell_essid
+                print "WIFI_QUALITY[" cell_num "]=" cell_quality
+            }
+        }'
+}
